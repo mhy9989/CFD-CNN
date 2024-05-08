034b353f5033e4251bf6fb148647b877011ed005
mhy9989 <1121267940@qq.com>
2024-05-08 15:41:32 +0800

    fix name

--- MSTA/MSTA.py
+++ MSTA/MSTA.py
@@ -33,7 +33,7 @@ class Multiple_Spatiotemporal_AttentionLayer(nn.Module):
         attn = self.conv0(x)           # depth-wise conv
         attn = self.conv_spatial(attn) # depth-wise dilation convolution
         f_x = self.conv1(attn)         # 1x1 conv
-        # append a MCA
+        # append a MFA
         b, c, _, _ = x.size()
         x_h = self.pool_h(x).squeeze(-1).permute(0,2,1)         # for W AvgPool, (B, C, H, 1) -> (B, H, C)
         x_w = self.pool_w(x).squeeze(-2).permute(0,2,1)         # for H AvgPool, (B, C, 1, W) -> (B, W, C)
