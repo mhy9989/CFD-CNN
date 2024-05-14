1404a7f8ee7f2089b8023c19b22b24c0240548f6
mhy9989 <1121267940@qq.com>
2024-05-14 20:34:07 +0800

    set default env

--- utils/utils.py
+++ utils/utils.py
@@ -139,6 +139,8 @@ def set_seed(seed, deterministic=True):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+    os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':16:8'
     if deterministic:
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
